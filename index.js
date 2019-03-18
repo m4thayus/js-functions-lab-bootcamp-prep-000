63a7b5ce8dd3106dffc454796b16edf13e4d9caa
Matt Williams <matt.seongjun@gmail.com>
2019-03-18 21:38:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,8 @@ function happyHolidayTo(holiday, name) {
 function holidayCountdown(days, holiday) {
   `It's ${days} days until ${holiday}!`
 }
+
+happyHolidays()
+happyHolidaysTo("you")
+happyHolidayTo("Lunar New Year", "you")
+holidayCountdown(20, "Mother's Day")
